6af6b137e5f3311ab815d32cf922756fae3714d1
keanbarz <keanbareng@gmail.com>
2024-02-14 14:07:35 +0800

    enhanced mailing system

--- app/Console/Commands/EmailProcessingCommand.php
+++ app/Console/Commands/EmailProcessingCommand.php
@@ -37,19 +37,28 @@ class EmailProcessingCommand extends Command
         foreach ($folders as $folder) {
             // Get the folder name
             $folderName = basename($folder);
+            $subFolders = File::directories($folder);           
+            foreach ($subFolders as $subFolder){
+                $subFolderName = basename($subFolder);
 
-            // Specify the email address based on the folder name
-            $destinationEmail = $this->getEmailForFolder($folderName);
-            
-            \Log::info($folder);
+                // Specify the email address based on the folder name
+                $destinationEmail = $this->getEmailForFolder($folderName, $subFolderName);
+                
+                \Log::info($folder);
 
-            // Get all files in the folder
-            $files = File::allFiles($folder);
-            \Log::info($files);
-            
-            // Process the contents and send email
-            $subject = 'Unclaimed Transaction as of ' . date('m/d/Y') . ' (EXPERIMENTAL!)';  // Replace with your desired subject
-            $this->sendEmail($destinationEmail, $subject, $files);
+                // Get all files in the folder
+                $files = File::allFiles($subFolder);
+                \Log::info($files);
+                
+                // Process the contents and send email
+                $subject = '(!EXPERIMENT!) ' . $folderName . ' ' . $subFolderName . ' - Unclaimed Palawan Transaction/s as of ' . date('m/d/Y');  // Replace with your desired subject
+                $this->sendEmail($destinationEmail, $subject, $files);
+
+                if ($destinationEmail != 'dolexiremittance@gmail.com'){
+                    File::delete($files);
+                }
+
+            }
         }
     }    
 
@@ -59,7 +68,7 @@ class EmailProcessingCommand extends Command
      * @param string $folderName
      * @return string
      */
-    function getEmailForFolder($folderName)
+    function getEmailForFolder($folderName, $subFolderName)
     {
         // Add your logic to map folder names to email addresses
         // You can use a switch statement, an array lookup, or any other method
@@ -68,9 +77,77 @@ class EmailProcessingCommand extends Command
         switch ($folderName) 
         {
             case 'ROXI':
-                return 'dolexiremittance@gmail.com';
-            case 'sent':
-                return 'sent@example.com';
+                switch ($subFolderName){
+                    case 'TUPAD':
+                    return 'dolexiremittance@gmail.com';
+                    case 'SPES':
+                    return 'dolexiremittance@gmail.com';
+                    case 'GIP':
+                    return 'dolexiremittance@gmail.com';
+                }
+            case 'DCFO':
+                switch ($subFolderName){
+                    case 'TUPAD':
+                    return 'dolexiremittance@gmail.com';
+                    case 'SPES':
+                    return 'dolexiremittance@gmail.com';
+                    case 'GIP':
+                    return 'dolexiremittance@gmail.com';
+                }
+            case 'DNFO':
+                switch ($subFolderName){
+                    case 'TUPAD':
+                    return 'dolexiremittance@gmail.com';
+                    case 'SPES':
+                    return 'dolexiremittance@gmail.com';
+                    case 'GIP':
+                    return 'dolexiremittance@gmail.com';
+                }
+            case 'DIEO':
+                switch ($subFolderName){
+                    case 'TUPAD':
+                    return 'dolexiremittance@gmail.com';
+                    case 'SPES':
+                    return 'dolexiremittance@gmail.com';
+                    case 'GIP':
+                    return 'dolexiremittance@gmail.com';
+                }
+            case 'DORFO':
+                switch ($subFolderName){
+                    case 'TUPAD':
+                    return 'dolexiremittance@gmail.com';
+                    case 'SPES':
+                    return 'dolexiremittance@gmail.com';
+                    case 'GIP':
+                    return 'dolexiremittance@gmail.com';
+                }
+            case 'DOFO':
+                switch ($subFolderName){
+                    case 'TUPAD':
+                    return 'dolexiremittance@gmail.com';
+                    case 'SPES':
+                    return 'dolexiremittance@gmail.com';
+                    case 'GIP':
+                    return 'dolexiremittance@gmail.com';
+                }
+            case 'DSFO':
+                switch ($subFolderName){
+                    case 'TUPAD':
+                    return 'dolexiremittance@gmail.com';
+                    case 'SPES':
+                    return 'dolexiremittance@gmail.com';
+                    case 'GIP':
+                    return 'dolexiremittance@gmail.com';
+                }       
+            case 'DOCFO':
+                switch ($subFolderName){
+                    case 'TUPAD':
+                    return 'dolexiremittance@gmail.com';
+                    case 'SPES':
+                    return 'dolexiremittance@gmail.com';
+                    case 'GIP':
+                    return 'dolexiremittance@gmail.com';
+                }                 
             // Add more cases as needed
             default:
                 return 'default@example.com';
@@ -87,17 +164,23 @@ class EmailProcessingCommand extends Command
     function sendEmail($destinationEmail, $subject, $files)
     {
        // Check if the content is empty
-    /*if (empty($files)) {
+    if (empty($files)) {
         // Log an error or handle it as needed
         \Log::error('Email content is empty. Skipping sending email.');
         return;
-    }*/
+    }
 
     // Replace this with your email sending logic using Laravel's Mail facade
     // This is just a basic example, and you need to configure your email settings
     // in Laravel before using this in a production environment.
 
-    Mail::raw('', function ($message) use ($destinationEmail, $subject, $files) {
+    $messageBody = 'Good day!' . PHP_EOL . PHP_EOL; // Concatenating strings with a new line
+    $messageBody .= 'Plese see attached file for the List of Unclaimed Palawan Transactions.' . PHP_EOL; // Concatenating another string with a new line
+    $messageBody .= 'To view the file, please enter "To Be Determined" as the file password' . PHP_EOL . PHP_EOL;
+    $messageBody .= 'Thank you,' . PHP_EOL . 'NOVIE JANE B. PANIAGUA';
+
+
+    Mail::raw($messageBody, function ($message) use ($destinationEmail, $subject, $files) {
         $message->to($destinationEmail)
                 ->subject($subject);
     
